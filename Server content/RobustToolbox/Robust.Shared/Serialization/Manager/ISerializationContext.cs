c23ff68708df2af4ff51cb16c4435399876391a5
KotikeD <132345303+OMGazk88@users.noreply.github.com>
2023-05-05 12:12:22 +0300

    Delete5

--- Server content/RobustToolbox/Robust.Shared/Serialization/Manager/ISerializationContext.cs
+++
@@ -1,8 +0,0 @@
-namespace Robust.Shared.Serialization.Manager
-{
-    // TODO Serialization: make this actually not kanser to use holy moly (& allow generics)
-    public interface ISerializationContext
-    {
-        SerializationManager.SerializerProvider SerializerProvider { get; }
-    }
-}
