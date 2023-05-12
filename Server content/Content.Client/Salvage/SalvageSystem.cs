80c40a05bf8582fbdeba85a01583b56e70da5093
KotikeD <dopoi16.SS@gmail.com>
2023-05-12 22:29:35 +0300

    tuda2

--- Server content/Content.Client/Salvage/SalvageSystem.cs
+++
@@ -1,8 +0,0 @@
-using Content.Shared.Salvage;
-
-namespace Content.Client.Salvage;
-
-public sealed class SalvageSystem : SharedSalvageSystem
-{
-
-}
--- Server content/Content.Shared/Administration/Notes/SharedAdminNote.cs
+++
@@ -1,6 +0,0 @@
-﻿using Robust.Shared.Serialization;
-
-namespace Content.Shared.Administration.Notes;
-
-[Serializable, NetSerializable]
-public sealed record SharedAdminNote(int Id, int? Round, Guid Player, string Message, string CreatedByName, string EditedByName, DateTime CreatedAt, DateTime LastEditedAt);
