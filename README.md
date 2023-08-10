9fca117ed04d043bf2cb35e38555296455443ee4
BlasterA <141688029+BlasterA@users.noreply.github.com>
2023-08-10 18:57:54 +0300

    ctrl c + ctrl v

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bluespace-Station
-
-Добро пожаловать на наш GitHub. С самим проектом вы можете ознакомиться в https://github.com/fedKotikeD-org/Bluespace-Station/blob/main/INFO/GENERAL.txt
-
-
--- Tools/corvax/setup_secrets.ps1
+++
@@ -1,2 +0,0 @@
-﻿New-Item -ItemType Junction -Path "Resources\Prototypes\CorvaxSecrets" -Target "Secrets\Prototypes"
-New-Item -ItemType Junction -Path "Resources\Prototypes\CorvaxSecretsServer" -Target "Secrets\ServerPrototypes"
